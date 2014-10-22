e44b049254d734664ddc191c5a994383be6b63bc
Patrick Barnes <patrick478@gmail.com>
2014-10-22 17:13:25 +1300

    Fix syntax

--- db/migrate/20141022035159_create_song_listens.rb
+++ db/migrate/20141022035159_create_song_listens.rb
@@ -2,7 +2,7 @@ class CreateSongListens < ActiveRecord::Migration
   def change
     create_table :song_listens do |t|
 
-        t.integer :user_id, :song_id, :nods, 
+        t.integer :user_id, :song_id, :nods
         t.text :location
 
 
